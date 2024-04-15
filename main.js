0f75f87ab775de8a946b7dacd6d3789bd0b4bf00
Rom-Freedom <romeo.dark@mail.ru>
2024-04-15 23:05:17 +0300

    finish the footer section

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ menuBtn.addEventListener('click', (e) => {
     menuBtnIcon.setAttribute('class', isOpen ? 'ri-close-line' : 'ri-menu-line')
 });
 
+
 navLinks.addEventListener('click', (e) => {
     navLinks.classList.remove('open');
     menuBtnIcon.setAttribute('class', 'ri-menu-line');
